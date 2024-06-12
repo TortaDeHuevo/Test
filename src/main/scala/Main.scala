8d87b8d4d48310a234d814e090e3238cda09ff12
TortaDeHuevo <juanju2k19@gmail.com>
2024-06-11 19:24:34 -0600

    Second Commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
   }
 }
