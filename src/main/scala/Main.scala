78928dd4278ffda54e8ed3c21b78f51ba1da98e1
TortaDeHuevo <juanju2k19@gmail.com>
2024-06-11 21:03:53 -0600

    Se movio la funcion dentro de el objeto main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,15 @@
+import scala.io.StdIn._
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
+    ejercicio3()
+  }
+  def ejercicio3(): Unit = {
+    print("Hola bienvenido!\nPor favor ingresa el ancho de la habitacion en metros: ")
+    val ancho = readFloat()
+    print("Ingresa el largo de la habitacion en metros: ")
+    val largo = readFloat()
+    val res = largo * ancho
+    println(s"El area de la habitacion es de: $res metros")
   }
 }
+
