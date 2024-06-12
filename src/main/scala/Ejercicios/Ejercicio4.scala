a8d73893cab3525971ea258cc238dc82872b9950
TortaDeHuevo <juanju2k19@gmail.com>
2024-06-11 22:44:24 -0600

    second file

---
+++ src/main/scala/Ejercicios/Ejercicio4.scala
@@ -0,0 +1,5 @@
+package Ejercicios
+
+object Ejercicio4 extends App{
+  println("Hola Mundo")
+}
--- src/main/scala/Ejercicios/Main.scala
+++ src/main/scala/Ejercicios/Main.scala
@@ -1,3 +1,5 @@
+package Ejercicios
+
 import scala.io.StdIn._
 object Main {
   def main(args: Array[String]): Unit = {
